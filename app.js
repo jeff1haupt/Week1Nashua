286a724d97674304c0ff50d4d74df626b4b56a49
jeff1haupt <jhaupt@southbendin.gov>
2021-10-12 20:12:44 -0400

    new commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// var firstName = "Jeff";
+// console.log(firstName);
+// console.log("This printed!");
+// const lastName = "Haupt"; 
+
+// let myAge = 46; 
+
+
+
+// var addTwoNumbers = 50 + 40;
+// console.log(addTwoNumbers / 0);
+// console.log( 1/ 0);
+// console.log(addTwoNumbers + 10);
+// let myBirthYear = 2021 - myAge; 
+// console.log(myBirthYear / 0);
+// var divideTwoNumbs = 50 / 10;
+
+// let multiplyTwoNumbs = 5 * 10; 
+
+console.log("1" + 1);
+
+console.log("Hello" + 1);
+
+console.log("Hello" * 3);
+
+let fullName = "Jeff Haupt"; 
+
+if ( fullName != undefined ) {
+    let isTrue = true;
+    console.log(isTrue);
+}
